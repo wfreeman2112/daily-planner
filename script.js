ca914365c1de0477a6170c26c1bc6c2fcc3232a4
Wendi Freeman <wfreeman2112@gmail.com>
2020-10-28 17:59:18 -0500

    cleaning up style

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /* get API for time and date
-make table in jquery adding time blocks
+make table 
 time blocks color coded for past, present and future
 save events in local storage
 */
 
-
+//establishing day and date at the top
 $(document).ready(function() {
     $('.dayAndDate').html(moment().format('MMMM DD YYYY'))
 
+
+    //color coding past, present and future
     $(".hour").each(function(){
         var hour = $(this).attr("hour");
         $(this).children("textarea").val(localStorage.getItem(hour));
@@ -24,6 +26,8 @@ $(document).ready(function() {
             $(this).children("textarea").addClass("future")
         }
     })
+
+    //storing input info
     $("button").click(function() {
         var hour = $(this).parent().attr("hour");
         var value = $(this).prev().val();
